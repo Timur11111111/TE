e0a9bad4e2cac07b6d20368ac169b62ddaea607a
Timur <newmew@tutanota.com>
2024-09-07 13:28:01 +0500

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+# #!/bin/bash
+
+# # Выполнение миграций
+# python manage.py makemigrations --noinput
+
+# # Применение миграций
+# python manage.py migrate --noinput
+
+# # Сбор статических файлов
+# python manage.py collectstatic --noinput
+
+# # Запуск сервера Django
+# python manage.py runserver 0.0.0.0:8000
+
+
+
+#!/bin/bash
+
+# Выполнение миграций, сборка статических файлов и загрузка данных
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+# Загрузка данных из JSON файла
+python manage.py loaddata my_data.json
+
+# Запуск Stripe CLI для прослушивания вебхуков в фоновом режиме
+stripe listen --forward-to localhost:8000/payment/webhook/ &
+
+# Запуск Django сервера
+python manage.py runserver 0.0.0.0:8000
+
