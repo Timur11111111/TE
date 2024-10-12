03982f83f7d1e026a797b239ae6e4754470e3068
Timur <newmew@tutanota.com>
2024-10-12 22:36:43 +0500

    allout3

--- Jinjer/settings.py
+++ Jinjer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k287=lxf*p&3^4swov^$o4_2$9rya-^nu@3p5i98ymdh@zu^r-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['power.aslan19p.uz', 'localhost', '127.0.0.1']
 
 
 # Application definition
